91365070bde62d2aa893a7db7ccda115e1ff47c9
Crafty <42453713+CCraftY@users.noreply.github.com>
2022-12-29 11:10:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ DKTIME = ''
 class report:
     def __init__(self):
         chrome_options = Options()
-        chrome_options.add_argument('lang=ja')
+        chrome_options.add_argument('--lang=ja')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
